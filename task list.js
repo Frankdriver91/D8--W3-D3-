b8cbfa1ec17dc8d439291ea4479f1209002c8c0e
Francesco Guida <francguida91@gmail.com>
2023-10-11 18:20:15 +0200

    css

--- task list.js
+++ task list.js
@@ -8,9 +8,11 @@ const btnClicked = function (e) {
   e.target.innerText = "Task aggiunto!";
 };
 
-const btn1 = document.getElementById("button");
-btn1.addEventListener("click", btnClicked1);
-console.log(btn1);
+const button1 = document.getElementById("buttonInput");
+button1.onclick = function writetask() {
+  const task1Input = document.getElementById("task1");
+};
+console.log(button1);
 
 const task1Input = document.getElementById("task1");
 const task2Input = document.getElementById("task2");
@@ -18,13 +20,6 @@ const task3Input = document.getElementById("task3");
 const task4Input = document.getElementById("task4");
 const task5Input = document.getElementById("task5");
 const task6Input = document.getElementById("task6");
-
-const task1 = task1Input.value;
-const task2 = task2Input.value;
-const task3 = task3Input.value;
-const task4 = task4Input.value;
-const task5 = task5Input.value;
-const task6 = task6Input.value;
 
 button.onclick = function (e) {
   const btnClicked = e.target;
